f5dabe1503c1b2b7d7e2ebeeb1ff33936b81b70b
Asanka W.P. Perera <asanka.wpp@gmail.com>
2020-03-28 12:30:18 -0600

    add /

--- src/main/java/com/abc/auto/MaintenanceController.java
+++ src/main/java/com/abc/auto/MaintenanceController.java
@@ -10,11 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.abc.auto.model.Vehicle;
@@ -41,7 +37,13 @@ public class MaintenanceController implements WebMvcConfigurer {
 	public ModelAndView home(Model model) {
 		return new ModelAndView("redirect:/vehicles/viewVehicles");
 	}
-	
+
+	@GetMapping("/")
+	@ResponseBody
+	public String home(){
+		return "Howdy world !!";
+	}
+
 	@GetMapping("/addVehicles")
 	public String showform(Model model){
 		model.addAttribute("vehicle", new Vehicle());
